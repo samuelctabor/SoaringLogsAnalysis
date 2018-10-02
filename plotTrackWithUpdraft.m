f1e9c2a6c00de15a74cbb37b48b12e4531fbba78
Sam Tabor <sam.tabor@kitepowersystems.com>
2018-10-02 23:05:24 +0100

    Allow plotTrackWithUpdraft.m to work with only the basic logged variables.

--- plotTrackWithUpdraft.m
+++ plotTrackWithUpdraft.m
@@ -3,14 +3,14 @@ function plotTrackWithUpdraft(FlightData, addTimeLabels)
     % Plot aircraft position
     figure;
     hold on;
-    plot3(FlightData.EstPosM(:,1),FlightData.EstPosM(:,2),FlightData.alt,'b');
+    plot3(FlightData.estPosE,FlightData.estPosN,FlightData.alt,'b');
 
     % Plot measured updraft as colours
-    av = mean( FlightData.FilterInputs(:,1));
-    dev = std( FlightData.FilterInputs(:,1));
+    av = mean( FlightData.nettorate);
+    dev = std( FlightData.nettorate);
     clims =[0,av+2*dev];
 
-    inputs_clipped = FlightData.FilterInputs(:,1);
+    inputs_clipped = FlightData.nettorate;
     inputs_clipped(inputs_clipped>clims(2)) = clims(2);
     inputs_clipped(inputs_clipped<clims(1)) = clims(1);
 
@@ -23,7 +23,7 @@ function plotTrackWithUpdraft(FlightData, addTimeLabels)
 
     for i=1:numel(FlightData.Time)
         c=A(round(colours(i)*(size(A,1)-1))+1,:);
-        plot3(FlightData.AircraftPosition(i,1),FlightData.AircraftPosition(i,2),FlightData.alt(i),'o','MarkerFaceColor',c,'MarkerEdgeColor',c);   % AC position
+        plot3(FlightData.posE(i),FlightData.posN(i),FlightData.alt(i),'o','MarkerFaceColor',c,'MarkerEdgeColor',c);   % AC position
     end
 
     grid on;
@@ -41,6 +41,6 @@ function plotTrackWithUpdraft(FlightData, addTimeLabels)
     if addTimeLabels
         idx = 1:100:length(FlightData.Time);
         labels = arrayfun(@(x)datestr(x,'MM:SS'),FlightData.Time(idx)-FlightData.Time(1),'UniformOutput',false);
-        text(FlightData.AircraftPosition(idx,1),FlightData.AircraftPosition(idx,2),FlightData.alt(idx),labels);
+        text(FlightData.posE(idx),FlightData.posN(idx),FlightData.alt(idx),labels);
     end
 end
