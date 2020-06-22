78f1151d33fc09bd16b8498e0a0dcdff2183d0a1
Samuel Tabor <samuelctabor@gmail.com>
2020-06-22 22:22:13 +0100

    Allow loadLog to take a file path.

--- loadLog.m
+++ loadLog.m
@@ -1,25 +1,30 @@
-function log = loadLog()
+function log = loadLog(filepath)
 % Select a log using UI, saving location for next call.
 
-    persistent fpath;
-    
-    if isempty(fpath) || length(fpath)==1 || ~exist(fpath,'dir')
-        fpath = pwd;
-    end
-    
-    [fname,fpathOut,~] = uigetfile({'*.log;*.mat;*.BIN'},'Select',fpath);
-    
-    if fname==0
-        log = [];
-    else
-        fpath = fpathOut;
-        fprintf('%s %s\n', fname,fpath);
+    if nargin<1
+        persistent fpath;
 
-        aplog = Ardupilog(fullfile(fpath,fname));
+        if isempty(fpath) || length(fpath)==1 || ~exist(fpath,'dir')
+            fpath = pwd;
+        end
 
-        log = aplog.getStruct();
-        log = fillNKF1(log);
-        log = NormaliseTimes(log);
-    end
+        [fname,fpathOut,~] = uigetfile({'*.log;*.mat;*.BIN'},'Select',fpath);
+
+        if fname==0
+            log = [];
+            return;
+        else
+            fpath = fpathOut;
+            fprintf('%s %s\n', fname,fpath);
+
+            filepath = fullfile(fpath,fname);
+        end
+    end 
+
+    aplog = Ardupilog(filepath);
+
+    log = aplog.getStruct();
+    log = fillNKF1(log);
+    log = NormaliseTimes(log);
 end
 
