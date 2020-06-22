85424e613652c35a3acb5212de67b84f52376491
Samuel Tabor <samuelctabor@gmail.com>
2020-06-22 22:21:49 +0100

    Re-work latlng_to_m.

--- latlong_to_m.m
+++ latlong_to_m.m
@@ -1,11 +1,9 @@
-function northeast=latlong_to_m(lonlat, reflat)
-    %R = 0.011131884502145034;
-    %north = R*latlon(:,1);
-    %east = R*cos(deg2rad(1e-7*latlon(:,1))).*latlon(:,2);
-    %northeast=[north,east];
-    sc = cos(deg2rad(reflat*1e-7));
-    north = lonlat(:,2)* 0.0111318845;
-    east = lonlat(:,1).* (0.0111318845*sc);
+function east_north=latlong_to_m(lonlat, reference_latitude)
+
+    sc = cos(deg2rad(reference_latitude*1e-7));
+    
+    north = lonlat(:,2) *  0.0111318845;
+    east  = lonlat(:,1).* (0.0111318845*sc);
    
-    northeast=[east,north];
+    east_north=[east,north];
 end
